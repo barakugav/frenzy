293e2dcefbf9919ef5f8d3fccea46c67cec45ad6
Barak Ugav <barakugav@gmail.com>
2025-12-01 16:53:52 +0200

    Simpler bytes hash, dont call memcpy

--- src/xor.rs
+++ src/xor.rs
@@ -12,42 +12,48 @@ impl BuildHasher for XorHash {
 
 pub(crate) struct XorHasher(u64);
 impl Hasher for XorHasher {
+    #[inline(always)]
     fn write_u64(&mut self, i: u64) {
         self.0 ^= i as u64;
     }
 
+    #[inline(always)]
     fn write(&mut self, bytes: &[u8]) {
-        let mut chunks = bytes.chunks_exact(8);
-        while let Some(chunk) = chunks.next() {
-            let chunk: [u8; 8] = unsafe { chunk.try_into().unwrap_unchecked() };
-            let chunk = u64::from_ne_bytes(chunk);
-            self.write_u64(chunk);
+        let (mut bytes, mut len) = (bytes.as_ptr(), bytes.len());
+        while len > 8 {
+            self.write_u64(unsafe { bytes.cast::<u64>().read_unaligned() });
+            bytes = unsafe { bytes.add(8) };
+            len -= 8;
         }
-
-        let mut remainder = [0_u8; 8];
-        remainder[..chunks.remainder().len()].copy_from_slice(chunks.remainder());
-        let remainder = u64::from_ne_bytes(remainder);
-        self.write_u64(remainder);
+        for i in 0..len {
+            self.0 ^= unsafe { *bytes.add(i) as u64 } << (i * 8);
+        }
     }
 
+    #[inline(always)]
     fn finish(&self) -> u64 {
         let hash = self.0;
         hash ^ (hash >> 33) ^ (hash >> 15)
     }
 
+    #[inline(always)]
     fn write_u8(&mut self, i: u8) {
         self.write_u64(i as u64);
     }
+    #[inline(always)]
     fn write_u16(&mut self, i: u16) {
         self.write_u64(i as u64);
     }
+    #[inline(always)]
     fn write_u32(&mut self, i: u32) {
         self.write_u64(i as u64);
     }
+    #[inline(always)]
     fn write_u128(&mut self, i: u128) {
         self.write_u64(unsafe { (&i as *const u128).cast::<u64>().add(0).read() });
         self.write_u64(unsafe { (&i as *const u128).cast::<u64>().add(1).read() });
     }
+    #[inline(always)]
     fn write_usize(&mut self, i: usize) {
         self.write_u64(i as u64);
     }
