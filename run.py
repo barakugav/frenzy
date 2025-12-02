4443677efba0a208adfa0ff98fe355c1df7c95ee
Barak Ugav <barakugav@gmail.com>
2025-12-02 07:22:40 +0200

    Keep debug symbols when profiling with samply

--- run.py
+++ run.py
@@ -7,11 +7,18 @@ parser.add_argument("--samply", action="store_true")
 args = parser.parse_args()
 
 proj_dir = Path(__file__).parent.resolve()
-subprocess.check_call(["cargo", "build", "--release", "--quiet"], cwd=proj_dir)
+
+if args.samply:
+    exe = ["samply", "record"]
+    profile = "profiling"
+else:
+    exe = ["time"]
+    profile = "release"
+
 subprocess.check_call(
-    [
-        *(["samply", "record"] if args.samply else ["time"]),
-        proj_dir / "target" / "release" / "frenzy",
-    ],
+    ["cargo", "build", f"--profile={profile}", "--quiet"], cwd=proj_dir
+)
+subprocess.check_call(
+    [*exe, proj_dir / "target" / profile / "frenzy"],
     cwd=proj_dir,
 )
